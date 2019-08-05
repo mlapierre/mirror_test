7e89264b3f52794698a875093c30aa9bf73024b7
Mark Lapierre <mlapierre@gitlab.com>
2019-08-05 05:12:06 +0000

    Upload New File

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,10 @@
+﻿
+# Load posh-git example profile
+. 'C:\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
+
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
